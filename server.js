ad15061cb00dce4e4e9304a2e72d6bddbb8bcf59
Ivan Vlasov <vlasoff.ivan95@gmail.com>
2023-06-01 22:58:51 +0300

    add basic middleware

--- server.js
+++ server.js
@@ -7,9 +7,28 @@ app.get("/users", (req, res) => {
   res.send([{ id: 1 }, { id: 2 }]);
 });
 
+app.post(
+  "/users",
+  (req, res, next) => {
+    console.log("first middleware");
+    next()
+  },
+  (req, res, next) => {
+    console.log("second middleware");
+    next()
+  },
+  (req, res) => {
+    console.log("final func");
+    res.send("user created");
+  }
+);
 
-  app.get("/test*", (req, res) => {
-    res.send(`request.path is ${req.path} and requests.method`);
-  });
+app.get("/test*", (req, res) => {
+  res.send(`request.path is ${req.path} and requests.method`);
+});
+
+// app.post("/users");
+// app.put("/users");
+// app.delete("/users");
 
 app.listen(PORT);
