ff000d0ab4b66f747fcdb3cd24ff2e3d9d01792b
Ivan Vlasov <vlasoff.ivan95@gmail.com>
2023-06-07 20:40:05 +0300

    separate express app from server

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const { validateUserMW } = require("./middlewares/userMW");
+const usersController = require("./controllers/user.controller");
+const app = express();
+const bodyParser = express.json();
+app.get("/users", usersController.getUsers);
+app.get("/users/:userId", usersController.getUser);
+app.post("/users", bodyParser, validateUserMW, usersController.createUser);
+app.delete("/users/:userId", usersController.deleteUser);
+app.put("/users/:userId", bodyParser, usersController.updateUser);
+
+module.exports = app;
--- server.js
+++ server.js
@@ -1,15 +1,5 @@
-const express = require("express");
-const { validateUserMW } = require("./middlewares/userMW");
-const usersController = require("./controllers/user.controller");
-
-const app = express();
+const http = require("http");
+const app = require('./app')
+const server = http.createServer(app);
 const PORT = 5001;
-const bodyParser = express.json();
-
-app.get("/users", usersController.getUsers);
-app.get("/users/:userId", usersController.getUser);
-app.post("/users", bodyParser, validateUserMW, usersController.createUser);
-app.delete("/users/:userId", usersController.deleteUser);
-app.put("/users/:userId", bodyParser, usersController.updateUser);
-
-app.listen(PORT);
+server.listen(PORT);
