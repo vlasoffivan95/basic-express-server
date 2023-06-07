9c56c0871800effb662931a85350ad089a608393
Ivan Vlasov <vlasoff.ivan95@gmail.com>
2023-06-07 21:14:38 +0300

    create app route

--- app.js
+++ app.js
@@ -3,11 +3,23 @@ const { validateUserMW } = require("./middlewares/userMW");
 const usersController = require("./controllers/user.controller");
 const app = express();
 // const bodyParser = express.json();
-app.use(express.json())
-app.get("/users", usersController.getUsers);
-app.get("/users/:userId", usersController.getUser);
-app.post("/users", validateUserMW, usersController.createUser);
-app.delete("/users/:userId", usersController.deleteUser);
-app.put("/users/:userId", usersController.updateUser);
+app.use(express.json());
+
+app
+  .route("/users")
+  .get(usersController.getUsers)
+  .post(validateUserMW, usersController.createUser);
+
+app
+  .route("/users/:userId")
+  .get(usersController.getUser)
+  .delete(usersController.deleteUser)
+  .put(usersController.updateUser);
+
+// app.get("/users", usersController.getUsers);
+// app.get("/users/:userId", usersController.getUser);
+// // app.post("/users", validateUserMW, usersController.createUser);
+// app.delete("/users/:userId", usersController.deleteUser);
+// app.put("/users/:userId", usersController.updateUser);
 
 module.exports = app;
