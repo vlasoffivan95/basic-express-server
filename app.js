43b5b3fd0eaee5543cb57b82234aeb3ba7399824
Ivan Vlasov <vlasoff.ivan95@gmail.com>
2023-06-07 20:45:48 +0300

    add method app.use()

--- app.js
+++ app.js
@@ -2,11 +2,12 @@ const express = require("express");
 const { validateUserMW } = require("./middlewares/userMW");
 const usersController = require("./controllers/user.controller");
 const app = express();
-const bodyParser = express.json();
+// const bodyParser = express.json();
+app.use(express.json())
 app.get("/users", usersController.getUsers);
 app.get("/users/:userId", usersController.getUser);
-app.post("/users", bodyParser, validateUserMW, usersController.createUser);
+app.post("/users", validateUserMW, usersController.createUser);
 app.delete("/users/:userId", usersController.deleteUser);
-app.put("/users/:userId", bodyParser, usersController.updateUser);
+app.put("/users/:userId", usersController.updateUser);
 
 module.exports = app;
